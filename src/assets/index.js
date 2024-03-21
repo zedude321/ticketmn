09281b97a51cbeb7f4975f1d02809cebfbebe4c3
Zedude321 <zedude321@gmail.com>
2024-03-21 11:22:37 +0800

    added card

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,5 @@ export * from "./user-icon";
 export * from "./google-icon";
 export * from "./eye-icon";
 export * from "./eye-hide-icon";
+export * from "./location-icon";
+export * from "./calendar-icon";
